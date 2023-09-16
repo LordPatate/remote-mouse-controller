a459cbbe54040f5b667aaf472615c21ac2f0ec0e
Tom Bricaud <tom.bricaud1@orange.fr>
2023-09-16 16:10:01 +0200

    Wrap client in main

--- client.py
+++ client.py
@@ -2,20 +2,26 @@ import socket
 
 HOST, PORT = "localhost", 9999
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    # Connect to server
-    sock.connect((HOST, PORT))
-    while True:
-        try:
-            data = input("> ")
-        except EOFError:
-            break
-        sock.sendall(bytes(data + "\n", "utf-8"))
-        try:
-            received = str(sock.recv(1024), "utf-8")
-        except (ConnectionAbortedError, ConnectionResetError):
-            print("Connection closed by server")
-            break
-        print(received)
-    print("Shutting down.")
+
+def main():
+    # Create a socket (SOCK_STREAM means a TCP socket)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        # Connect to server
+        sock.connect((HOST, PORT))
+        while True:
+            try:
+                data = input("> ")
+            except EOFError:
+                break
+            sock.sendall(bytes(data + "\n", "utf-8"))
+            try:
+                received = str(sock.recv(1024), "utf-8")
+            except (ConnectionAbortedError, ConnectionResetError):
+                print("Connection closed by server")
+                break
+            print(received)
+        print("Shutting down.")
+
+
+if __name__ == "__main__":
+    main()
